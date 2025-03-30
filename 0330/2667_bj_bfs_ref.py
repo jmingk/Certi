34ae804c1858601dcb368785db1c7268c7a5219d
Da-Eun Oh <76278018+daeunohh@users.noreply.github.com>
2025-03-30 13:38:17 +0000

    2667 bfs ref

---
+++ 0330/2667_bj_bfs_ref.py
@@ -0,0 +1,37 @@
+#bfs
+import sys
+sys.stdin = open("/workspaces/Certi/0330/2667_input.txt", "r")
+
+from collections import deque
+
+def bfs(graph, startx, starty, n):
+    dx = [-1, 1, 0, 0]
+    dy = [0, 0, -1, 1]
+    count = 1
+    q = deque([[startx,starty]])
+    graph[startx][starty] = 0
+
+    while q:
+        x, y = q.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if 0 <= nx < n and 0 <= ny < n and graph[nx][ny] == 1:                
+                count += 1
+                q.append([nx, ny])
+                graph[nx][ny] = 0
+    return count
+
+n = int(input())
+graph = [list(map(int, input())) for _ in range(n)]  # 지도 정보
+
+result = []
+for i in range(n):
+    for j in range(n):
+        if graph[i][j] == 1:
+            result.append(bfs(graph, i, j, n))
+
+result.sort()
+print(len(result))
+for size in result:
+    print(size)
