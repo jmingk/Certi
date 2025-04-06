74659a5a6a3599f036aec66fa00c47c3ff4b6a05
Da-Eun Oh <76278018+daeunohh@users.noreply.github.com>
2025-04-06 08:38:47 +0000

    1238 answer 2

--- 0406/1238_bj_answer.py
+++ 0406/1238_bj_answer.py
@@ -6,38 +6,41 @@ import heapq
 N, M, X = map(int, input().split(' '))
 INF = int(1e9)
 
-def dijk(start):
-    table = [INF] * (N + 1)
-    table[start] = 0
+def dijk(start, graph):
+    distance = [INF] * (N + 1)
+    distance[start] = 0
 
-    q = []
-    heapq.heappush(q, [table[start], start])
-    while q:
-        _, node = heapq.heappop(q)
-        for nnode in graph[node]:
-            nextnode, dist = nnode
-            if table[nextnode] > table[node] + dist:
-                table[nextnode] =  table[node] + dist
-                heapq.heappush(q, [table[nextnode], nextnode])
-    return table
+    heap = [[distance[start], start]]
+    while heap:
+        dist, node = heapq.heappop(heap)
+        if distance[node] < dist: continue
+
+        for nextnode, cost in graph[node]:
+            if distance[nextnode] > dist + cost:
+                distance[nextnode] =  dist + cost
+                heapq.heappush(heap, [distance[nextnode], nextnode])
+    return distance
 
 graph = dict()
+rev_graph = dict()
 for _ in range(M):
     src, dest, dist = map(int, input().split(' '))
     if src in graph:
         graph[src].append([dest, dist])
     else:
         graph[src] = [[dest, dist]]
+    if dest in rev_graph:
+        rev_graph[dest].append([src, dist])
+    else:
+        rev_graph[dest] = [[src, dist]]
 
-dists = [0] * (N + 1)
+maxdist = 0
+dist_toX = dijk(X, rev_graph)
+dist_fromX = dijk(X, graph)
+
 for i in range(1, N + 1):
-    table = dijk(i)
-    dists[i] = table[X]
+    maxdist = max(maxdist, dist_toX[i] + dist_fromX[i])
 
-table = dijk(X)
-for i in range(1, N + 1):
-    dists[i] += table[i]
-
-print(max(dists))
+print(maxdist)
 
 
