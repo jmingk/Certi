c409ab9f10d288f8a9d1d9510bc6956d980c1feb
jmingk <mingk1991@gmail.com>
2025-04-13 19:14:36 +0900

    programers carpet update
    -

---
+++ 0413/carpet.py
@@ -0,0 +1,20 @@
+def solution(brown, yellow):
+    total = brown + yellow
+
+    for width in range(3, total + 1):
+        if total % width == 0:
+            height = total // width
+            if width >= height and (width - 2) * (height - 2) == yellow:
+                return [width, height]
+
+# def solution(brown, yellow):
+#     total = brown + yellow
+#     possible_sizes = []
+#
+#     for width in range(1, total + 1):
+#         if total % width == 0:
+#             height = total // width
+#             if width >= height:
+#                 possible_sizes.append((width, height))
+#
+#     return list(possible_sizes[-1])
