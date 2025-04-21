e5ea3cdbb9ac090e1d0ee1edae7c93677992595a
Da-Eun Oh <76278018+daeunohh@users.noreply.github.com>
2025-04-21 06:17:53 +0000

    0420

--- 0420/carcenter_answer.py
+++ 0420/carcenter_answer.py
@@ -7,9 +7,9 @@ for t in range(1,T+1):
     n,m,k,A,B = map(int, input().split()) # 접수창구 수, 정비창구 수, 고객 수
     A -= 1
     B -= 1
-    접수창구 = list(map(int, input().split())) # 각 접수창구 처리시간
-    정비창구 = list(map(int, input().split())) # 각 정비창구 처리시간
-    도착시간 = list(map(int, input().split())) # 각 고객의 도착시간
+    reception_list = list(map(int, input().split())) # 각 접수창구 처리시간
+    repaircenter_list = list(map(int, input().split())) # 각 정비창구 처리시간
+    arrival_time_list = list(map(int, input().split())) # 각 고객의 도착시간
 
     heapA = []
     for i in range(n):
@@ -22,19 +22,19 @@ for t in range(1,T+1):
     temp = []
     users = [] # 접수창구를 이용한 고객들
     for i in range(k):
-        time = 도착시간[i] # 현재 고객의 도착시간
+        time = arrival_time_list[i] # 현재 고객의 도착시간
         while heapA and heapA[0][0] <= time: # 끝나는시간이 도착시간보다 작으면 바로 진행 가능
             end_time, idx = heapq.heappop(heapA) # 해당 접수 (끝나는 시간,접수번호)
             heapq.heappush(temp, idx) # 어차피 접수 번호만 알면 돼
 
         if temp: # 이용 가능한 접수창구 존재 -> 번호 낮은 애들 순으로 저장 중.
             idx = heapq.heappop(temp)
-            end_time = time + 접수창구[idx] # 현재 고객이 접수창구가 끝나는 시간
+            end_time = time + reception_list[idx] # 현재 고객이 접수창구가 끝나는 시간
             heapq.heappush(users, (end_time, idx, i)) # (접수 창구 끝나는 시간, 접수창구번호, 현재 이용한 고객 인덱스)
             heapq.heappush(heapA, (end_time, idx))
         else: # 이용 가능한 접수창구 없으면..
             end_time, idx = heapq.heappop(heapA) # 가장 빨리 끝나는 접수창구
-            end_time += 접수창구[idx] # 가장 먼저 끝나는 접수창구 시간 + 접수창구 시간
+            end_time += reception_list[idx] # 가장 먼저 끝나는 접수창구 시간 + 접수창구 시간
             heapq.heappush(users, (end_time, idx, i))
             heapq.heappush(heapA, (end_time, idx))
     # 정비창구 시작
@@ -49,12 +49,12 @@ for t in range(1,T+1):
 
         if temp: # 이용 가능한 정비창구 존재
             idxB = heapq.heappop(temp)
-            end_timeB = end_timeA + 정비창구[idxB]  # 정비창구 끝나는 시간
+            end_timeB = end_timeA + repaircenter_list[idxB]  # 정비창구 끝나는 시간
             heapq.heappush(heapB, (end_timeB, idxB))
             res.append((userIdx, idxA,idxB))
         else: # 이용 가능한 정비 창구가 없다면
             end_timeB, idxB = heapq.heappop(heapB) # 가장 먼저 끝나는 정비창구, 인덱스
-            end_timeB += 정비창구[idxB]
+            end_timeB += repaircenter_list[idxB]
             heapq.heappush(heapB, (end_timeB, idxB))
             res.append((userIdx, idxA,idxB))
 
