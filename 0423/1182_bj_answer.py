08f470b3e1bf3e7f56337dd6616b7a2355e020f9
Da-Eun Oh <76278018+daeunohh@users.noreply.github.com>
2025-04-23 07:18:07 +0000

    partial sum ver 3

--- 0423/1182_bj_answer.py
+++ 0423/1182_bj_answer.py
@@ -3,27 +3,28 @@ import sys
 import heapq
 sys.stdin = open("0423/input.txt", "rt")
 
-# ver 2: cur_sum 캐싱 추가 
-# 결과 : 34924 KB	380 ms
-from collections import deque
+# ver 3: seq 제거
+# 결과 : 34908 KB	228 ms
 
 N, target = list(map(int, input().split(' ')))
 ns = list(map(int, input().split(' ')))
 cnt = 0
 
-def dfs(seq = [], i=0, cur_sum = 0):
+def dfs(i=0, cur_sum = 0):
     global N, target, ns, cnt
-    # print(seq)
 
     if i >= N :
-        if len(seq) > 0 and (cur_sum) == target:
+        if cur_sum == target:
             # print('#####target')
             cnt += 1
             return
         return
     
-    dfs(seq, i + 1, cur_sum)
-    dfs(seq + [ns[i]], i + 1, cur_sum + ns[i])
+    dfs(i + 1, cur_sum)
+    dfs(i + 1, cur_sum + ns[i])
 
-dfs([], 0, 0)
+if target == 0:
+    cnt -= 1
+
+dfs(0, 0)
 print(cnt)
