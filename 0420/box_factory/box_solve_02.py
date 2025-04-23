f891020f16d24cbad8f971f07ec678855c2a7f85
jmingk <mingk1991@gmail.com>
2025-04-23 22:03:06 +0900

    box_factory_caching_update
    -

--- 0420/box_factory/box_solve_02.py
+++ 0420/box_factory/box_solve_02.py
@@ -3,23 +3,47 @@ boxes = [3, 10, 5, 2, 8, 100, 7, 4]
 
 max_value = 0
 
-def dfs(index, a_list, b_list, a_sum, b_sum):
+def dfs(index, last_a, last_b, a_sum, b_sum):
     global max_value
 
-    if index == N-1:
-        if a_list and b_list:
+    if index == N:
+        if last_a is not None and last_b is not None:  # A와 B가 최소 1개 이상 선택된 경우만 유효
             max_value = max(max_value, a_sum + b_sum)
         return
 
     current_box = boxes[index]
 
-    if not a_list or current_box >= a_list[-1]:
-        dfs(index + 1, a_list + [current_box], b_list, a_sum + current_box, b_sum)
+    if last_a is None or current_box >= last_a:
+        dfs(index + 1, current_box, last_b, a_sum + current_box, b_sum)
 
-    if not b_list or current_box <= b_list[-1]:
-        dfs(index + 1, a_list, b_list + [current_box], a_sum, b_sum + current_box)
+    if last_b is None or current_box <= last_b:
+        dfs(index + 1, last_a, current_box, a_sum, b_sum + current_box)
 
-    dfs(index + 1, a_list, b_list, a_sum, b_sum)
+    dfs(index + 1, last_a, last_b, a_sum, b_sum)
 
-dfs(0, [], [], 0, 0)
+dfs(0, None, None, 0, 0)
 print(max_value)
+
+
+# max_value = 0
+#
+# def dfs(index, a_list, b_list, a_sum, b_sum):
+#     global max_value
+#
+#     if index == N:
+#         if a_list and b_list:
+#             max_value = max(max_value, a_sum + b_sum)
+#         return
+#
+#     current_box = boxes[index]
+#
+#     if not a_list or current_box >= a_list[-1]:
+#         dfs(index + 1, a_list + [current_box], b_list, a_sum + current_box, b_sum)
+#
+#     if not b_list or current_box <= b_list[-1]:
+#         dfs(index + 1, a_list, b_list + [current_box], a_sum, b_sum + current_box)
+#
+#     dfs(index + 1, a_list, b_list, a_sum, b_sum)
+#
+# dfs(0, [], [], 0, 0)
+# print(max_value)
